007ff77a01a53c46b07346f13b24a0831209f753
Junichi Ito <jit@sonicgarden.jp>
2013-04-26 09:32:59 +0900

    Enable to choose year and month

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -1,17 +1,21 @@
 require 'date'
 
 class CalendarRenderer
+  def initialize(year, month)
+    @first_date = Date.new year, month, 1
+  end
+
   def render
-    "#{header}#{body}"
+    "#{header}\n#{body}"
   end
 
   private
 
   def header
-   <<EOS
-     April 2013
-Su Mo Tu We Th Fr Sa
-EOS
+    sun_to_sat = "Su Mo Tu We Th Fr Sa"
+    month_year = first_date.strftime "%B %Y"
+    month_year_centered_and_trimmed = month_year.center(sun_to_sat.length).gsub(/\s+$/, '')
+    "#{month_year_centered_and_trimmed}\n#{sun_to_sat}"
   end
 
   def body
@@ -44,11 +48,11 @@ EOS
   end
 
   def first_date
-    Date.new(2013, 4, 1)
+    @first_date
   end
 
   def last_date
-    Date.new(2013, 4, 30)
+    @first_date.next_month.prev_day
   end
 end
 
