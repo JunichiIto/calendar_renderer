75ba4d184173c0f2df19400caaffffa7aa9869a4
Junichi Ito <jit@sonicgarden.jp>
2013-04-26 21:17:04 +0900

    Write in one method

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -6,40 +6,24 @@ class CalendarRenderer
   end
 
   def to_s
-    calendar_rows.join("\n")
-  end
-
-  private
-
-  def calendar_rows
-    header_rows + body_rows
-  end
-
-  def header_rows
     sun_to_sat = "Su Mo Tu We Th Fr Sa"
     month_year = @first_date.strftime("%B %Y").center(sun_to_sat.size).rstrip
-    [month_year, sun_to_sat]
-  end
+    header_rows = [month_year, sun_to_sat]
 
-  def body_rows
-    format_row = -> week {
-      # if Rails => week.map {|date| date.try(:strftime, "%e") || "  " }.join(" ")
-      week.map {|date| date.nil? ? "  " : date.strftime("%e") }.join(" ")
-    }
-    weeks_in_month.map {|week| format_row.call(week) }
-  end
+    last_date = @first_date.next_month.prev_day
+    dates_in_month = (@first_date..last_date).to_a
 
-  def weeks_in_month
-    dates_in_month.inject([]) {|weeks, date|
+    weeks_in_month = dates_in_month.inject([]) {|weeks, date|
       weeks << [] if weeks.empty? or date.sunday?
       weeks.tap {|weeks| weeks.last[date.wday] = date }
     }
-  end
 
-  def dates_in_month
-    # if Rails => (@first_date..@first_date.end_of_month).to_a
-    last_date = @first_date.next_month.prev_day
-    (@first_date..last_date).to_a
+    format_row = -> week {
+      week.map {|date| date.nil? ? "  " : date.strftime("%e") }.join(" ")
+    }
+    body_rows = weeks_in_month.map {|week| format_row.call(week) }
+
+    (header_rows + body_rows).join("\n")
   end
 end
 
