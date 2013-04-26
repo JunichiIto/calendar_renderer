9566ba429d79c43426e7c80feb4ef6680eb353ad
Junichi Ito <jit@sonicgarden.jp>
2013-04-26 10:35:56 +0900

    Refactoring with lambda

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -14,19 +14,14 @@ class CalendarRenderer
   def header
     sun_to_sat = "Su Mo Tu We Th Fr Sa"
     month_year = first_date.strftime "%B %Y"
-    "#{rtrim month_year.center(sun_to_sat.size)}\n#{sun_to_sat}"
-  end
-
-  def rtrim(str)
-    str.gsub(/\s+$/, '')
+    rtrim = -> str { str.gsub(/\s+$/, '') }
+    "#{rtrim.call(month_year.center(sun_to_sat.size))}\n#{sun_to_sat}"
   end
 
   def body
-    month_table.map {|week| week_to_string(week) }.join("\n")
-  end
-  
-  def week_to_string(week)
-    week.map {|date| date.nil? ? "  " : date.strftime("%e") }.join(" ")
+    date_to_s = -> date { date.nil? ? "  " : date.strftime("%e") }
+    week_to_s = -> week { week.map {|date| date_to_s.call(date) }.join(" ") }
+    month_table.map {|week| week_to_s.call(week) }.join("\n")
   end
 
   def month_table
@@ -45,7 +40,7 @@ class CalendarRenderer
   end
 
   def last_date
-    @first_date.next_month.prev_day
+    first_date.next_month.prev_day
   end
 end
 
