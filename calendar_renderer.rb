7d092eee777f607e0050657baaf550249b8e7682
Junichi Ito <jit@sonicgarden.jp>
2013-04-26 22:12:57 +0900

    Refactoring shorter

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -9,9 +9,9 @@ class CalendarRenderer
       [first_date.strftime("%B %Y").center(sun_to_sat.size).rstrip, sun_to_sat] +
       # Body
       (first_date..first_date.next_month.prev_day).to_a.inject([]) {|weeks, date|
-        weeks << [] if weeks.empty? or date.sunday?
-        weeks.tap {|weeks| weeks.last[date.wday] = date }
-      }.map {|week| week.map {|date| date.nil? ? "  " : date.strftime("%e") }.join(" ") }
+        weeks << Array.new(7, "  ") if weeks.empty? or date.sunday?
+        weeks.tap {|weeks| weeks.last[date.wday] = date.strftime("%e") }
+      }.map {|week| week.join(" ").rstrip }
     ).join("\n")
   end
 end
