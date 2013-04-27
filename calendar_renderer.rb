9689bb37c2cc8dc604311d2c49e22d91d4f0dd86
Junichi Ito <jit@sonicgarden.jp>
2013-04-28 07:05:20 +0900

    Remove some ()

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -10,7 +10,7 @@ class CalendarRenderer
 
     (
       [first_date.strftime("%B %Y").center(week_length + indent_length).rstrip, " Su Mo Tu We Th Fr Sa"] +
-      ((1..31).to_a.map{|i| i.to_s.rjust(day_length) }.join)[0..(first_date.next_month.prev_day.day * day_length - 1)].unpack("a#{week_length - first_date.wday * day_length}" + "a#{week_length}" * week_count_in_month).tap {|rows|
+      (1..31).to_a.map{|i| i.to_s.rjust(day_length) }.join[0..first_date.next_month.prev_day.day * day_length - 1].unpack("a#{week_length - first_date.wday * day_length}" + "a#{week_length}" * week_count_in_month).tap {|rows|
         rows[0] = rows[0].rjust(week_length)
       }
     ).join("\n")
