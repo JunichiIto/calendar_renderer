ce97f8e9fc5c84a793e6a150d7dbff4853aeb29e
Junichi Ito <jit@sonicgarden.jp>
2013-04-28 06:11:31 +0900

    Remove constants

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -3,9 +3,6 @@ require 'date'
 class CalendarRenderer
   DAY_LENGTH = 3
   WEEK_LENGTH = DAY_LENGTH * 7
-  INDENT_LENGTH = 1
-  MONTH_RANGE = 1..31
-  WEEK_COUNT_IN_MONTH = 5
 
   def initialize(year, month)
     @first_date = Date.new(year, month, 1)
@@ -18,8 +15,9 @@ class CalendarRenderer
   private
 
   def body_rows
+    week_count_in_month = 5
     first_week_offset = WEEK_LENGTH - @first_date.wday * DAY_LENGTH
-    split_template = "a#{first_week_offset}" + "a#{WEEK_LENGTH}" * WEEK_COUNT_IN_MONTH
+    split_template = "a#{first_week_offset}" + "a#{WEEK_LENGTH}" * week_count_in_month
 
     this_month_calendar.unpack(split_template).tap {|rows|
       rows[0] = rows[0].rjust(WEEK_LENGTH)
@@ -39,11 +37,12 @@ class CalendarRenderer
   end
 
   def full_length_calendar
-    MONTH_RANGE.to_a.map{|i| i.to_s.rjust(DAY_LENGTH) }.join
+    (1..31).to_a.map{|i| i.to_s.rjust(DAY_LENGTH) }.join
   end
 
   def header_rows
-    month_year = @first_date.strftime("%B %Y").center(WEEK_LENGTH + INDENT_LENGTH).rstrip
+    indent_length = 1
+    month_year = @first_date.strftime("%B %Y").center(WEEK_LENGTH + indent_length).rstrip
     sun_to_sat = " Su Mo Tu We Th Fr Sa"
     [month_year, sun_to_sat]
   end
