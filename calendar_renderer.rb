f61b29e440bcbf379891f94cdf5bd6f9a667fcc2
Junichi Ito <jit@sonicgarden.jp>
2013-04-28 06:25:13 +0900

    Remove constants

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -1,27 +1,26 @@
 require 'date'
 
 class CalendarRenderer
-  DAY_LENGTH = 3
-  WEEK_LENGTH = DAY_LENGTH * 7
-
   def self.render(year, month)
+    day_length = 3
+    week_length = day_length * 7
     first_date = Date.new(year, month, 1)
-
     indent_length = 1
-    month_year = first_date.strftime("%B %Y").center(WEEK_LENGTH + indent_length).rstrip
+    week_count_in_month = 5
+
+    month_year = first_date.strftime("%B %Y").center(week_length + indent_length).rstrip
     sun_to_sat = " Su Mo Tu We Th Fr Sa"
     header_rows = [month_year, sun_to_sat]
 
-    week_count_in_month = 5
-    first_week_offset = WEEK_LENGTH - first_date.wday * DAY_LENGTH
-    split_template = "a#{first_week_offset}" + "a#{WEEK_LENGTH}" * week_count_in_month
+    first_week_offset = week_length - first_date.wday * day_length
+    split_template = "a#{first_week_offset}" + "a#{week_length}" * week_count_in_month
 
     last_date = first_date.next_month.prev_day
-    this_month_length = last_date.day * DAY_LENGTH - 1
-    full_length_calendar = (1..31).to_a.map{|i| i.to_s.rjust(DAY_LENGTH) }.join
+    this_month_length = last_date.day * day_length - 1
+    full_length_calendar = (1..31).to_a.map{|i| i.to_s.rjust(day_length) }.join
     this_month_calendar = full_length_calendar[0..this_month_length]
     body_rows = this_month_calendar.unpack(split_template).tap {|rows|
-      rows[0] = rows[0].rjust(WEEK_LENGTH)
+      rows[0] = rows[0].rjust(week_length)
     }
 
     (header_rows + body_rows).join("\n")
