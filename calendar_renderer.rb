7e566b18b000bf8f89fc6e91dacc3420ddd1afa1
Junichi Ito <jit@sonicgarden.jp>
2013-04-28 05:56:18 +0900

    Remove arguments by using instance variable

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -8,40 +8,33 @@ class CalendarRenderer
   WEEK_COUNT_IN_MONTH = 5
 
   def initialize(year, month)
-    @year = year
-    @month = month
+    @first_date = Date.new(year, month, 1)
   end
 
   def to_s
-    render(@year, @month)
+    (header_rows + calendar_rows).join("\n")
   end
 
   private
 
-  def render(year, month)
-    first_date = Date.new(year, month, 1)
-    
-    (header_rows(first_date) + calendar_rows(first_date)).join("\n")
-  end
-
-  def calendar_rows(first_date)
-    first_week_offset = WEEK_LENGTH - first_date.wday * DAY_LENGTH
+  def calendar_rows
+    first_week_offset = WEEK_LENGTH - @first_date.wday * DAY_LENGTH
     template = "a#{first_week_offset}" + "a#{WEEK_LENGTH}" * WEEK_COUNT_IN_MONTH
 
-    this_month_calendar(first_date).unpack(template).tap {|rows|
+    this_month_calendar.unpack(template).tap {|rows|
       rows[0] = rows[0].rjust(WEEK_LENGTH)
     }
   end
 
-  def this_month_calendar(first_date)
-    last_date = Date.new(first_date.year, first_date.month, -1)
+  def this_month_calendar
+    last_date = Date.new(@first_date.year, @first_date.month, -1)
     full_length_calendar = MONTH_RANGE.to_a.map{|i| i.to_s.rjust(DAY_LENGTH) }.join
     this_month_length = last_date.day * DAY_LENGTH - 1
     full_length_calendar[0..this_month_length]
   end
 
-  def header_rows(first_date)
-    month_year = first_date.strftime("%B %Y").center(WEEK_LENGTH + INDENT_LENGTH).rstrip
+  def header_rows
+    month_year = @first_date.strftime("%B %Y").center(WEEK_LENGTH + INDENT_LENGTH).rstrip
     sun_to_sat = " Su Mo Tu We Th Fr Sa"
     [month_year, sun_to_sat]
   end
