8ded58576d3e21b36ba7c91bd0c9a8ea1703ad7f
Junichi Ito <jit@sonicgarden.jp>
2013-04-28 06:59:14 +0900

    Shorten logic

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -8,21 +8,11 @@ class CalendarRenderer
     indent_length = 1
     week_count_in_month = 5
 
-    month_year = first_date.strftime("%B %Y").center(week_length + indent_length).rstrip
-    sun_to_sat = " Su Mo Tu We Th Fr Sa"
-    header_rows = [month_year, sun_to_sat]
-
-    first_week_offset = week_length - first_date.wday * day_length
-    split_template = "a#{first_week_offset}" + "a#{week_length}" * week_count_in_month
-
-    last_date = first_date.next_month.prev_day
-    this_month_length = last_date.day * day_length - 1
-    full_length_calendar = (1..31).to_a.map{|i| i.to_s.rjust(day_length) }.join
-    this_month_calendar = full_length_calendar[0..this_month_length]
-    body_rows = this_month_calendar.unpack(split_template).tap {|rows|
-      rows[0] = rows[0].rjust(week_length)
-    }
-
-    (header_rows + body_rows).join("\n")
+    (
+      [first_date.strftime("%B %Y").center(week_length + indent_length).rstrip, " Su Mo Tu We Th Fr Sa"] +
+      ((1..31).to_a.map{|i| i.to_s.rjust(day_length) }.join)[0..(first_date.next_month.prev_day.day * day_length - 1)].unpack("a#{week_length - first_date.wday * day_length}" + "a#{week_length}" * week_count_in_month).tap {|rows|
+        rows[0] = rows[0].rjust(week_length)
+      }
+    ).join("\n")
   end
 end
