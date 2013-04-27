fed5045050dc00509a88b2a6801bb91ccff0feff
Junichi Ito <jit@sonicgarden.jp>
2013-04-28 05:53:16 +0900

    Join rows at single point

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -21,12 +21,13 @@ class CalendarRenderer
   def render(year, month)
     first_date = Date.new(year, month, 1)
     
-    render_header(first_date) + calendar_rows(first_date).join("\n")
+    (header_rows(first_date) + calendar_rows(first_date)).join("\n")
   end
 
   def calendar_rows(first_date)
     first_week_offset = WEEK_LENGTH - first_date.wday * DAY_LENGTH
     template = "a#{first_week_offset}" + "a#{WEEK_LENGTH}" * WEEK_COUNT_IN_MONTH
+
     this_month_calendar(first_date).unpack(template).tap {|rows|
       rows[0] = rows[0].rjust(WEEK_LENGTH)
     }
@@ -39,9 +40,9 @@ class CalendarRenderer
     full_length_calendar[0..this_month_length]
   end
 
-  def render_header(first_date)
+  def header_rows(first_date)
     month_year = first_date.strftime("%B %Y").center(WEEK_LENGTH + INDENT_LENGTH).rstrip
     sun_to_sat = " Su Mo Tu We Th Fr Sa"
-    "#{month_year}\n#{sun_to_sat}\n"
+    [month_year, sun_to_sat]
   end
 end
