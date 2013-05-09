366d30c214b76913f0e7010b6104185b7a6cbc0b
Junichi Ito <jit@sonicgarden.jp>
2013-05-10 06:45:39 +0900

    Refactoring

--- calendar_renderer.rb
+++ calendar_renderer.rb
@@ -7,6 +7,6 @@ class CalendarRenderer
     week_length = day_length * 7
     indent_length = 1
 
-    ([first_date.strftime("%B %Y").center(week_length + indent_length).rstrip, " Su Mo Tu We Th Fr Sa"] + (' ' * first_date.wday * day_length +(1..first_date.next_month.prev_day.day).to_a.map{|i| i.to_s.rjust(day_length) }.join).scan(/.{#{day_length},#{week_length}}/)).join("\n")
+    ([first_date.strftime("%B %Y").center(week_length + indent_length).rstrip, " Su Mo Tu We Th Fr Sa"] + (' ' * first_date.wday * day_length +(1..first_date.next_month.prev_day.day).map{|i| i.to_s.rjust(day_length) }.join).scan(/.{#{day_length},#{week_length}}/)).join("\n")
   end
 end
